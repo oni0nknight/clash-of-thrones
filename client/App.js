41f5d945ccb4b138aec235bf0e7cef450c1a36fd
Théophile Gindre <theophile.gindre@gmail.com>
2018-07-22 15:21:41 +0200

    Add bootstrap for landing page + prepare host/find games

--- client/App.js
+++ client/App.js
@@ -1,6 +1,28 @@
 import Game from './js/Game'
+import $ from 'jquery'
 
 window.onload = () => {
-    document.getElementById('title').innerHTML = 'init'
-    const game = new Game('test', 'red')
+    const form = $('#form')
+    $('#host-btn').on('click', e => {
+        form.addClass('was-validated')
+
+        if (form[0].checkValidity()) {
+            hostGame()
+        }
+    })
+    $('#find-btn').on('click', e => {
+        form.addClass('was-validated')
+
+        if (form[0].checkValidity()) {
+            findGame()
+        }
+    })
+}
+
+function hostGame() {
+
+}
+
+function findGame() {
+
 }
