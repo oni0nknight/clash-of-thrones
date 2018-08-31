379e4eb86916c42fa57430f6f33fc64f218924a2
Theophile Gindre <tge2@3ds.com>
2018-08-31 17:49:39 +0200

    [Client] add a map to store the subscribes in client to seak for subscribe leaks

--- client/js/Client.js
+++ client/js/Client.js
@@ -7,6 +7,8 @@ export default class Client {
         this.socket.on('err', err => {
             console.error(err)
         })
+
+        this.activeSubscriptions = []
     }
 
     call(eventName, args) {
@@ -18,18 +20,18 @@ export default class Client {
             let timeout = 0
 
             const handleResponse = result => {
-                this.socket.off(eventName+'_response', handleResponse)
+                this.unsubscribe(eventName+'_response', handleResponse)
                 clearTimeout(timeout)
                 resolve(result)
             }
             const handleError = () => {
-                this.socket.off(eventName+'_response', handleResponse)
+                this.unsubscribe(eventName+'_response', handleResponse)
                 clearTimeout(timeout)
                 reject()
             }
 
             // subscribe to response handler
-            this.socket.on(eventName+'_response', handleResponse)
+            this.subscribe(eventName+'_response', handleResponse)
 
             // emit the query
             this.socket.emit(eventName, args)
@@ -42,10 +44,15 @@ export default class Client {
     subscribe(eventName, callback) {
         if (callback instanceof Function) {
             this.socket.on(eventName, callback)
+            activeSubscriptions.push({eventName, callback})
         }
     }
 
     unsubscribe(eventName, callback) {
         this.socket.off(eventName, callback)
+        const idx = activeSubscriptions.findIndex(actsub => (actsub.eventName === eventName && actsub.callback === callback))
+        if (idx !== -1) {
+            activeSubscriptions.splice(idx, 1)
+        }
     }
 }
