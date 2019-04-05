3a130930bf9f6b7569ce4066cfd554f083cb57bd
Théophile Gindre <theophile.gindre@gmail.com>
2019-04-05 13:06:51 +0200

    Added 1st React components

--- client/App.js
+++ client/App.js
@@ -1,123 +1,19 @@
-import $ from 'jquery'
-import Client from './js/Client'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header/Header'
+import Container from './components/Container/Container'
 
-import InitState from './js/States/InitState'
-import LoaderState from './js/States/LoaderState'
-import HostLobbyState from './js/States/HostLobbyState'
-import JoinLobbyState from './js/States/JoinLobbyState'
-import WaitForPlayerState from './js/States/WaitForPlayerState'
-import GameState from './js/States/GameState'
-
-const client = new Client()
-client.subscribe('err', errorHandler)
-
-let stateStack = []
-let currentState = null
-
-let errorContainer = null
-
-const states = {
-    'init': null,
-    'loader': null,
-    'hostLobby': null,
-    'joinLobby': null,
-    'waitForPlayer': null,
-    'game': null
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Container>
+                <p>Content</p>
+            </Container>
+        </>
+    )
 }
 
 window.onload = () => {
-    // load states
-    states.init = new InitState(client)
-    states.loader = new LoaderState(client)
-    states.hostLobby = new HostLobbyState(client)
-    states.joinLobby = new JoinLobbyState(client)
-    states.waitForPlayer = new WaitForPlayerState(client)
-    states.game = new GameState(client)
-
-    errorContainer = $('.error-container')
-
-    // hide everything
-    $('#header').hide()
-    $('.state').hide()
-
-    // bind events
-    bindEvents()
-
-    if (window.location.search.includes('dbg_hst')) {
-        $('#playerName').text('player1')
-        $('#factionSelect').html('<option selected value="targaryens">targaryens</option>')
-        client.call('register', { name: 'player1', faction: 'targaryens' })
-        setTimeout(() => client.call('createGame', 'game'), 100)
-        setTimeout(() => switchToState('waitForPlayer', true), 200)
-    }
-    else if (window.location.search.includes('dbg_join')) {
-        $('#playerName').text('player1')
-        $('#factionSelect').html('<option selected value="targaryens">targaryens</option>')
-        client.call('register', { name: 'player2', faction: 'targaryens' })
-        switchToState('waitForPlayer', false)
-        client.query('pendingGames').then(pg => {
-            client.call('joinGame', pg[0].id)
-        })
-    }
-    else {
-        // start with 1st state
-        switchToState('init')
-    }
-}
-
-function bindEvents() {
-    document.addEventListener('switchState', e => {
-        switchToState(e.detail.state, ...e.detail.args)
-    })
-
-    document.addEventListener('leaveGame', e => {
-        stateStack = ['init']
-        switchToState(e.detail.state, ...e.detail.args)
-    })
-
-    document.addEventListener('rollbackState', e => {
-        // remove current state from stack
-        stateStack.pop()
-
-        // find the new state to enter to (cannot be loader state)
-        let newState = ''
-        do {
-            newState = stateStack.pop() // remove from stack
-        } while (newState === 'loader')
-
-        // switch to new state
-        switchToState(newState)
-    })
-}
-
-function switchToState(stateId, ...args) {
-    if (currentState) {
-        currentState.leave()
-    }
-    const state = states[stateId]
-    if (state) {
-        state.enter(...args)
-        stateStack.push(stateId)
-        currentState = state
-    }
-    else {
-        console.error('This state does not exist')
-    }
-}
-
-
-// Error handling
-//=====================================
-
-function errorHandler(error) {
-    if (errorContainer) {
-        let html = '<div class="alert alert-danger" role="alert" style="display:none;">'
-        html += (error.code + ' :: ' + error.msg)
-        html += '</div>'
-        errorContainer.append(html)
-
-        errorContainer.find(':last-child').fadeIn(150).delay(2000).fadeOut()
-
-        errorContainer.scrollTop(errorContainer.prop('scrollHeight'))
-    }
+    ReactDOM.render(<App />, document.getElementById('app'))
 }
---
+++ client/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+    <div id="header" className="jumbotron text-center">
+        <h1>Clash of Thrones</h1>
+        <p>Welcome to Clash of Thrones!</p>
+    </div>
+)
+
+export default Header
